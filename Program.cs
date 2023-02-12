1d19c4ab2f1c7adb0c17702d121c29174363909e
Shree Thakre <shreet365@gmail.com>
2023-02-12 18:25:05 +0530

    Adding Welcome Massage

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ***************** WELCOME TO LOGICAL PROGRAMS **************** ");
+            Console.ReadLine(); 
+        }
+    }
+}
